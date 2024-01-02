40f5b1594854d03b567806b0ff7a9ea6bae79ca6
stevn999 <14356815+stevn999@users.noreply.github.com>
2024-01-01 20:50:04 -0600

    formatting changes, and added a check so buttons don't get updated if they are already in the correct state.

--- src/com.novistrum.ksp2flightcontrol.sdPlugin/app.js
+++ src/com.novistrum.ksp2flightcontrol.sdPlugin/app.js
@@ -71,37 +71,7 @@ const images = {
 
 
 $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
-	console.log('Stream Deck connected!');
-    
-    // try {
-    //     const requestBody = {
-    //         "ID": generateRandomString(6),
-    //         "Action": "getShiptelemetry",
-    //         "parameters": {
-                
-    //         }
-    //     };
-
-    //     fetch('http://127.0.0.1:8080/api', {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         },
-    //         body: JSON.stringify(requestBody)
-    //     })
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         /// Handle the response data as needed
-	// 		console.log("Data Returned: ", data);
-
-    //     })
-    //     .catch(error => {
-    //         console.error('Error sending POST request:', error);
-    //     });
-    // } catch (error) {
-    //     console.error('Error constructing JSON:', error);
-    // }
-
+    console.log('Stream Deck connected!');
 });
 
 
@@ -114,7 +84,7 @@ function generateRandomString(length) {
     let randomString = '';
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
-        randomString += characters.charAt(randomIndex);
+        randomString += characters[randomIndex];
     }
     return randomString;
 };
@@ -122,7 +92,7 @@ function generateRandomString(length) {
 
 
 function imgToBase64(url, callback) {
-    const img = new Image();
+    let img = new Image();
     img.crossOrigin = 'Anonymous';
     img.onload = function () {
         const canvas = document.createElement('canvas');
@@ -140,7 +110,7 @@ function imgToBase64(url, callback) {
 }
 
 function imgToBG(context, type, name, status) {
-    var imageUrl = "";
+    let imageUrl = "";
 
     // Check if status is defined and not falsy
     if (status) {
@@ -152,7 +122,7 @@ function imgToBG(context, type, name, status) {
     if (imageUrl) {
         imgToBase64(imageUrl, (error, base64Img) => {
             if (error) {
-                 console.error('Error loading image:', error);
+                console.error('Error loading image:', error);
             } else {
                 // console.log("Updating image on Context:", context);
                 $SD.setImage(context, base64Img);
@@ -176,13 +146,13 @@ function imgToBG(context, type, name, status) {
 
 /// Function to handle adding a new button to the buttons array
 function addButton(context, name) {
-    const type = checkType(name);
+    let type = checkType(name);
     // Check if a button with the same context already exists
-    const existingButton = buttons.find(button => button.context === context);
-    
+    let existingButton = buttons.find(button => button.context === context);
+
     if (!existingButton) {
         // If no match is found, add the new button
-        buttons.push({ context, type, name, lastState:null});
+        buttons.push({ context, type, name, lastState: null });
         // console.log("Button added:", { context, type, name });
 
     } else {
@@ -194,9 +164,9 @@ function addButton(context, name) {
 
 /// Function to make the API request to get the action group state
 function getActionGroupState(button) {
-    const randomUser = generateRandomString(6);
+    let randomUser = generateRandomString(6);
 
-    const requestBody = {
+    let requestBody = {
         "ID": randomUser,
         "Action": "getActionGroupState",
         "parameters": {
@@ -211,22 +181,22 @@ function getActionGroupState(button) {
         },
         body: JSON.stringify(requestBody)
     })
-    .then(response => response.json())
-    .then(data => {
-        // console.log(button.name, " POST Returned State:", data.Data.Status);
-        return data.Data.Status;
-    
-    })
-    .catch(error => {
-        console.error('Error getting action group state:', error);
-        return null;
-    });
+        .then(response => response.json())
+        .then(data => {
+            // console.log(button.name, " POST Returned State:", data.Data.Status);
+            return data.Data.Status;
+
+        })
+        .catch(error => {
+            console.error('Error getting action group state:', error);
+            return null;
+        });
 };
 
 
 // Function to update the state of a single button
 async function updateSingleButtonState(context) {
-    const button = buttons.find(b => b.context === context);
+    let button = buttons.find(b => b.context === context);
     if (!button) {
         console.error('Button not found for context:', context);
         return;
@@ -234,11 +204,12 @@ async function updateSingleButtonState(context) {
 
     try {
         const status = await getActionGroupState(button);
-        if (status != null) {
+        if (status != null && status !== button.lastState) {
+            button.lastState = status;
             imgToBG(button.context, button.type, button.name, status);
         }
 
-        
+
     } catch (error) {
         console.error('Error updating button state:', error);
     }
@@ -248,10 +219,10 @@ async function updateSingleButtonState(context) {
 function checkType(name) {
     var type = "Other";
 
-    if ( name in images["ActionGroup"]["True"]) {
+    if (name in images["ActionGroup"]["True"]) {
         type = "ActionGroup";
-    } ;
-    if ( name in images["SAS"]) {
+    };
+    if (name in images["SAS"]) {
         type = "SAS";
     };
 
@@ -266,7 +237,7 @@ ActionGroupToggle.onKeyUp(({ context, payload }) => {
     console.log(payload.settings.action);
 
     /// Generate a random 6-character string
-    const randomUser = generateRandomString(6);
+    let randomUser = generateRandomString(6);
 
     try {
         const requestBody = {
@@ -284,26 +255,26 @@ ActionGroupToggle.onKeyUp(({ context, payload }) => {
             },
             body: JSON.stringify(requestBody)
         })
-        .then(response => response.json())
-        .then(data => {
-            /// Handle the response data as needed
-			console.log(payload.settings.action, " status on Key Up:", data.Data.Status);
-            console.log(payload.settings.action," Button Context: ", context)
+            .then(response => response.json())
+            .then(data => {
+                /// Handle the response data as needed
+                console.log(payload.settings.action, " status on Key Up:", data.Data.Status);
+                console.log(payload.settings.action, " Button Context: ", context)
 
-            var status = data.Data.Status
-            updateSingleButtonState(context, status);
-            
+                var status = data.Data.Status
+                updateSingleButtonState(context, status);
 
-        })
-        .catch(error => {
-            console.error('Error sending POST request:', error);
-        });
+
+            })
+            .catch(error => {
+                console.error('Error sending POST request:', error);
+            });
     } catch (error) {
         console.error('Error constructing JSON:', error);
     }
 });
 
-ActionGroupToggle.onDidReceiveSettings(({context, payload}) => {
+ActionGroupToggle.onDidReceiveSettings(({ context, payload }) => {
     var contextName = payload.settings.action
 
     if (contextName) {
@@ -312,14 +283,14 @@ ActionGroupToggle.onDidReceiveSettings(({context, payload}) => {
         addButton(context, contextName);
 
         console.log(contextName, 'Added');
-        
+
         buttons.forEach((button) => {
             if (context === button.context) {
                 var index = buttons.findIndex((button) => button.context === context);
                 button.name = contextName;
                 button.type = checkType(contextName);
-                
-                
+
+
             }
         });
     } else {
@@ -330,11 +301,11 @@ ActionGroupToggle.onDidReceiveSettings(({context, payload}) => {
 });
 
 ActionGroupToggle.onDialRotate(({ action, context, device, event, payload }) => {
-	console.log('Your dial code goes here!');
+    console.log('Your dial code goes here!');
 });
 
 ActionGroupToggle.onSendToPlugin(({ ActionGroup }) => {
-	console.log('Action Group Selection received');
+    console.log('Action Group Selection received');
 });
 
 
@@ -346,7 +317,7 @@ function startUpdateInterval() {
             const status = getActionGroupState(button);
             updateSingleButtonState(button.context, status);
         });
-    }, 2000); // Run every 1000 milliseconds (1 second)
+    }, 2000); // Run every 2000 milliseconds (2 seconds)
 }
 
 function stopUpdateInterval() {
@@ -355,7 +326,7 @@ function stopUpdateInterval() {
 
 
 ActionGroupToggle.onWillAppear(({ context }) => {
-    $SD.getSettings(context);    
+    $SD.getSettings(context);
     startUpdateInterval();
 });
 
@@ -363,7 +334,7 @@ ActionGroupToggle.onWillAppear(({ context }) => {
 ActionGroupToggle.onWillDisappear(({ context }) => {
     stopUpdateInterval();
     const indexToRemove = buttons.findIndex(button => button.context === context);
-    
+
     if (indexToRemove !== -1) {
         buttons.splice(indexToRemove, 1);
     }
